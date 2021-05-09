f5c49de7c7c2dbfdeb1b0ed880def827db53f65e
Thomas Walker <thomasnwalker@yahoo.com>
2021-05-09 14:03:04 -0700

    testing git

--- Develop/script.js
+++ Develop/script.js
@@ -12,7 +12,7 @@ function generatePassword(){
   var lowerAlphabet = "abcdefghijklmnopqrstuvwxyz".split("");
   var upperAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
   var numberList = "0123456789".split("");
-  var specialCharList = "!@#$%^&*()-_=+`,.~?<>?][}{/".split(""); 
+  var specialCharList = "!@#$%^&*()-_=+`,.~?<>?][}{/\\\"".split(""); // need to use escape characters and add \ and "
 
   var passwordString = "";
   
@@ -24,15 +24,21 @@ function generatePassword(){
      
     while(passwordString.length < lengthPrompt){
 
-        if(lowercasePrompt){passwordString += lowerAlphabet[Math.floor(Math.random() * lowerAlphabet.length)]}
-        if (uppercasePrompt){passwordString += upperAlphabet[Math.floor(Math.random() * upperAlphabet.length)]}
-        if(numericPrompt){passwordString += numberList[Math.floor(Math.random() * numberList.length)]} 
-        if(specialCharPrompt){passwordString += specialCharList[Math.floor(Math.random() * specialCharList.length)]}
-    
-      }
-  
-// after the appropriate characters are compiled, we should split the string into an array to shuffle its characters
+        if(lowercasePrompt && passwordString.length < lengthPrompt ){passwordString += lowerAlphabet[Math.floor(Math.random() * lowerAlphabet.length)]}
+        if (uppercasePrompt && passwordString.length < lengthPrompt){passwordString += upperAlphabet[Math.floor(Math.random() * upperAlphabet.length)]}
+        if(numericPrompt && passwordString.length < lengthPrompt){passwordString += numberList[Math.floor(Math.random() * numberList.length)]} 
+        if(specialCharPrompt && passwordString.length < lengthPrompt){passwordString += specialCharList[Math.floor(Math.random() * specialCharList.length)]}
 
+
+      }
+
+      // load a new password 
+        
+      
+
+      // need to set a length check for the password after while loop iterates OR a for loop that transfers an appropriate length into a new array 
+// after the appropriate characters are compiled, we should split the string into an array to shuffle its characters
+    
     return passwordString;
   
 }
@@ -43,10 +49,10 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword(); //* result of function is assigned to password variable
-  var passwordText = document.querySelector("#password"); //* passwordText variable is assigned to the #password textarea element which displayszz
+  var passwordText = document.querySelector("#password"); //* passwordText variable is assigned to the #password textarea element which displays
   passwordText.value = password;
   
 }
-
+ 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
